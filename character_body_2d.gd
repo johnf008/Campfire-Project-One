17435ad52a79bd2d04262494371f294e7c97db42
John <Carlos.flores.h108@gmail.com>
2026-01-28 10:12:04 -0600

    trying to figure out animations

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -3,7 +3,6 @@ extends CharacterBody2D
 const MAX_SPEED = 100
 var last_direction := Vector2(1,0)
 var fishing_ability = false
-
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * MAX_SPEED
@@ -12,6 +11,9 @@ func _physics_process(delta):
 	
 	if direction.length() > 0:
 		last_direction = direction
+	
+	print(direction)
+
 		
 	move_and_slide()
 
