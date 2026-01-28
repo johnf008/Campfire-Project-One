66d76e75940a2de494bc329159e65ccf4ca9e4ca
John <Carlos.flores.h108@gmail.com>
2026-01-27 22:01:32 -0600

    surrounding ocean has collision now

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -7,7 +7,9 @@ func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * MAX_SPEED
 	
-	move_and_slide()
+	
 	
 	if direction.length() > 0:
 		last_direction = direction
+		
+	move_and_slide()
