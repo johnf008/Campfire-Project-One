29acf775484743e2d12c8f4219b1e53deac295dc
John <Carlos.flores.h108@gmail.com>
2026-01-28 15:21:38 -0600

    ok it works but i want it to go back and forth

---
+++ game.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@export var event_bar: TextureRect
+
+
+func _on_event_bar_fail() -> void:
+	print("Bro you failed")
+	_reset()
+
+func _on_event_bar_green_done() -> void:
+	print("Did it land on green?")
+	_reset()
+
+func _on_event_bar_orange_done() -> void:
+	print("Did it land on orange?")
+	_reset()
+
+func _on_event_bar_regular_done() -> void:
+	print("Did it land on regular")
+	_reset()
+func _reset():
+	await get_tree().create_timer(1).timeout
+	event_bar.reset()
