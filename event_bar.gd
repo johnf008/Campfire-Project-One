3db67df793ff334780c1f2d94f0d979665d9ff06
John <Carlos.flores.h108@gmail.com>
2026-01-28 21:04:04 -0600

    multiplying the scale in the math did something but the indicator is still high in the sky

--- event_bar.gd
+++ event_bar.gd
@@ -14,19 +14,19 @@ var green_range_x = Vector2(8,13)
 var orange_range_x = Vector2(28, 36)
 
 func _ready() -> void:
-	x_end_position = global_position.x - indicator.size.x / 2
+	x_end_position = global_position.x - (indicator.size.x * scale.x) / 2
 	print("Bar size: ", size)
 	print("Indicator size: ", indicator.size)
 	start()
 
 func start():
-	var end_center_offset = Vector2(size.x, size.y / 2)
+	var end_center_offset = Vector2((size.x * scale.x), (size.y * scale.y) / 2)
 	var indicator_ceter_off_set = indicator.size / 2
 	
 	indicator.global_position = global_position + end_center_offset - indicator_ceter_off_set
 
 func _process(delta: float) -> void:
-	indicator.global_position.x -= size.x / duration * delta
+	indicator.global_position.x -= (size.x * scale.x) / duration * delta
 	
 	if indicator.global_position.x < x_end_position:
 		_finish(true)
@@ -54,7 +54,7 @@ func _finish(normalDone = false):
 	
 	
 func _in_range(event_range: Vector2):
-	var indicator_position = indicator.global_position.x + indicator.size.x / 2 - global_position.x
+	var indicator_position = indicator.global_position.x + (indicator.size.x * scale.x) / 2 - global_position.x
 	
 	if indicator_position >= event_range.x and indicator_position <= event_range.y:
 		return true
